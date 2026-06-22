efe334a3661390b0950cedc0a1a2f913988981fb
Viyathma Arukgoda <viyathmaawa@gmail.com>
2026-06-22 19:40:51 +0530

    Rename images and update project data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <TechSection />
-        <ProjectShowcase />
+        <ProjectShowcase /> 
         <CertificateSection />
         <ContactSection />
       </main>
